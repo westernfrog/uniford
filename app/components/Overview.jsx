b88bfeb809d4494ec710d2f5538f9103723178c4
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2024-03-11 22:51:52 +0530

    Update Overview.jsx

--- app/components/Overview.jsx
+++ app/components/Overview.jsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Info from "./Info";
 import MonthlyDonation from "./MonthlyDonation";
+import { Tab } from "@headlessui/react";
+import { Fragment } from "react";
 
 export default function Overview(params) {
   return (
@@ -32,8 +36,75 @@ export default function Overview(params) {
             </Link>
             <span>Check FAQ&apos;s</span>
           </div>
-          <hr className="py-2" />
-          <Info />
+          <Tab.Group>
+            <Tab.List>
+              <Tab as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={`
+w-[50%] rounded-sm py-3 font-semibold 
+                      ${
+                        selected
+                          ? "bg-[#B9E390] text-black"
+                          : "bg-transparent text-[#B9E390] border border-neutral-500"
+                      }`}
+                  >
+                    Individual
+                  </button>
+                )}
+              </Tab>
+              <Tab as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={`
+w-[50%] rounded-sm py-3 font-semibold
+                      ${
+                        selected
+                          ? "bg-[#B9E390] text-black"
+                          : "bg-transparent text-[#B9E390] border border-neutral-500"
+                      }`}
+                  >
+                    Organisation
+                  </button>
+                )}
+              </Tab>
+            </Tab.List>
+
+            <div className="py-4">
+              <label htmlFor="country" className="block font-medium leading-6">
+                How often would you like to donate?
+              </label>
+              <div className="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#B9E390] sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>Monthly</option>
+                  <option>Yearly</option>
+                  <option>Quaterly</option>
+                </select>
+              </div>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="flex items-center bg-[#B9E390]">
+                <label>HHH</label>
+                <input
+                  id="push-nothing"
+                  name="push-notifications"
+                  type="radio"
+                />
+              </div>
+            </div>
+            <hr className="py-2" />
+            <Tab.Panels>
+              <Tab.Panel>
+                <Info />
+              </Tab.Panel>
+              <Tab.Panel>Content 2</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
           <MonthlyDonation />
         </div>
       </main>
