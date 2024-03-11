2a694d5031b3dd96bf319c30f4098520f9ed7f57
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2024-03-12 00:41:58 +0530

    Updated

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600", "800"] });
 
 export const metadata = {
   title: "Uniford Foundation",
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Header />
         {children}
         <Footer />
