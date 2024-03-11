8a06fb90fce711747e6f073225e66efd4e922f42
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2024-03-11 19:44:00 +0530

    Updated

--- app/components/Container.jsx
+++ app/components/Container.jsx
@@ -1,7 +1,7 @@
 export default function Container({ children }) {
   return (
     <>
-      <main className="max-w-7xl mx-auto flex items-start gap-4 p-6">
+      <main className="max-w-7xl mx-auto flex lg:flex-row flex-col items-start gap-4 p-6">
         {children}
       </main>
     </>
